6a0e324a6e460a40f0402a9c3cc8bb7083b6dcdb
Raven Endless <103381750+TahmidRaven@users.noreply.github.com>
2024-11-15 21:34:45 +0600

    created pages and routes

--- xmart/src/App.jsx
+++ xmart/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import React from 'react'
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
 
 export default function App() {
-  return (
-  
-    <h1 className="text-4xl text-center">App</h1>
-  )
+
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>;
 }
